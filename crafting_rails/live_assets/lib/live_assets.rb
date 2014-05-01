b2f011200c8ad01beff1bb42b65c0c84ff61c236
Akira Osada <osd.akira@gmail.com>
2014-05-01 19:15:01 +0900

    thread

--- crafting_rails/live_assets/lib/live_assets.rb
+++ crafting_rails/live_assets/lib/live_assets.rb
@@ -1,4 +1,24 @@
 require "live_assets/engine"
+require "thread"
+require "listen"
 
 module LiveAssets
+  matter_reader :subscribers
+  @@subscribers = []
+
+  def self.subscribe(subscriber)
+    subscribers << subscriber
+  end
+
+  def self.unsubscribe(subscriber)
+    subscribers.delete(subscriber)
+  end
+
+  def self.start_listener(event, directories)
+    Thread.new do
+      Listen.to(*directories, latency: 0.5) do |_modified, _added, _removed|
+        subscribers.each { |s| s << event }
+      end
+    end
+  end
 end
