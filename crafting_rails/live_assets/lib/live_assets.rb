8e31f519d84d7a4a42180bfce142eabc459b55e5
Akira Osada <osd.akira@gmail.com>
2014-05-08 19:49:40 +0900

    99ページ

--- crafting_rails/live_assets/lib/live_assets.rb
+++ crafting_rails/live_assets/lib/live_assets.rb
@@ -3,22 +3,29 @@ require "thread"
 require "listen"
 
 module LiveAssets
-  matter_reader :subscribers
-  @@subscribers = []
-
-  def self.subscribe(subscriber)
-    subscribers << subscriber
-  end
-
-  def self.unsubscribe(subscriber)
-    subscribers.delete(subscriber)
-  end
-
-  def self.start_listener(event, directories)
-    Thread.new do
-      Listen.to(*directories, latency: 0.5) do |_modified, _added, _removed|
-        subscribers.each { |s| s << event }
-      end
-    end
-  end
+  autoload :SSESubscriber, "live_assets/sse_subscriber"
 end
+
+# module LiveAssets
+
+#   mattr_reader :subscribers
+#   @@subscribers = []
+
+#   def self.subscribe(subscriber)
+#     # puts "subscribe"
+#     subscribers << subscriber
+#   end
+
+#   def self.unsubscribe(subscriber)
+#     subscribers.delete(subscriber)
+#   end
+
+#   def self.start_listener(event, directories)
+#     # puts "start_listener"
+#     Thread.new do
+#       Listen.to(*directories, latency: 0.5) do |_modified, _added, _removed|
+#         subscribers.each { |s| s << event }
+#       end.start
+#     end
+#   end
+# end
--- crafting_rails/live_assets/lib/live_assets/engine.rb
+++ crafting_rails/live_assets/lib/live_assets/engine.rb
@@ -1,5 +1,15 @@
 module LiveAssets
   class Engine < ::Rails::Engine
-    paths["app/controllers"] = ["lib/controllers"]
+    # paths["app/controllers"] = ["lib/controllers"]
+    initializer "live_assets.start_listener" do |app|
+      paths = app.paths["app/assets"].existent +
+        app.paths["lib/assets"].existent +
+        app.paths["vendor/assets"].existent
+
+      paths = paths.select { |p| p =~ /stylesheets/ }
+      if app.config.assets.compile
+        LiveAssets.start_listener :reloadCSS, paths
+      end
+    end
   end
 end
